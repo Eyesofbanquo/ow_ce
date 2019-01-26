37cf21f050375ae8928aa3a96dcefe69c70ace87
mshaw <ms79723@gmail.com>
2019-01-26 00:41:38 -0600

    Create YamlParser
    - Create a YamlParser class file that is responsible
    for loading individual yaml files and printing out their keys

--- lib/yaml_parser.rb
+++ lib/yaml_parser.rb
@@ -1,4 +1,31 @@
 # lib/yaml_parser.rb
+require 'yaml'
+require 'json'
 
 class YamlParser
+
+    # Takes a file path to a .yaml/.yml file to parse into a hash
+    def self.parse(file_path)
+        unless file_path.empty? || file_path == ""
+            begin
+                yaml = YAML.load_file(file_path)
+                return yaml
+            rescue Errno::ENOENT => exception
+                return {}
+            end
+
+        else
+            return {}
+        end
+    end
+
+    # Takes a hash and returns an array of its keys
+    def self.union_keys(hash)
+        unless hash.empty?
+            return hash.keys
+        else
+            return []
+        end
+    end
+
 end
--- spec/yaml_parser_spec.rb
+++ spec/yaml_parser_spec.rb
@@ -2,4 +2,48 @@
 require 'yaml_parser'
 
 describe YamlParser do
+
+    describe "#parse" do
+        context "given an empty string" do
+            it "returns empty hash" do
+                expect(YamlParser.parse("")).to eql({})
+            end
+        end
+
+        context "given an incorrect file path" do
+            it "raises an error" do
+                expect(YamlParser.parse("./something.yaml")).to eql({})
+            end
+        end
+
+        context "given a correct file path" do
+            it "returns hash" do
+                expect(YamlParser.parse("./stubs/stub.yaml")).to_not eql({})
+            end
+
+            it "returns the name 'joe'" do
+                expect(YamlParser.parse("./stubs/stub.yaml")["name"]).to eql("joe")
+            end
+        end
+    end
+
+    describe "#union_keys" do
+        context "when given an empty hash" do
+            it "returns an empty array" do
+                expect(YamlParser.union_keys({})).to eql([])
+            end
+        end
+
+        context "when given a non empty hash" do 
+            hash = YamlParser.parse("./stubs/stub.yaml")
+            it "returns an array of keys" do
+                expect(YamlParser.union_keys(hash)).to_not eql([])
+            end
+
+            it "returns 3 elements" do
+                expect(YamlParser.union_keys(hash).size).to eql(3)
+            end
+        end
+    end
+
 end
