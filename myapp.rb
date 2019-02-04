7d800539fa5191ebb973ad3d6abc3488fd594ed3
mshaw <ms79723@gmail.com>
2019-02-03 22:05:11 -0600

    Add endpoints
    - Add rule endpoints for assault, control, escort, hybrid

--- myapp.rb
+++ myapp.rb
@@ -12,8 +12,32 @@ class MyApp < Sinatra::Base
 
   # Endpoint for retrieving rules
   namespace '/rules' do
+    require_relative 'lib/rules_loader'
+    include RulesLoader
+
+    get '/test-assault' do
+      json = load_json "data/sample/assault.json"
+      json.to_json
+    end
+
     get '/assault' do
-      "Assault rules"
+      json = load_json "data/modes/rules/assault.json"
+      json.to_json
+    end
+
+    get '/control' do
+      json = load_json "data/modes/rules/control.json"
+      json.to_json
+    end
+
+    get '/escort' do
+      json = load_json "data/modes/rules/escort.json"
+      json.to_json
+    end
+
+    get '/hybrid' do
+      json = load_json "data/modes/rules/hybrid.json"
+      json.to_json
     end
   end
 end
