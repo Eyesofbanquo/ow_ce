db52ae2c262f8ef86f5a765fbc5ae205969027a1
mshaw <ms79723@gmail.com>
2019-02-03 21:21:36 -0600

    Add sinatra-contrib gem
    - Add sinatra-contrib gem to Gemfile
    
    - Add namespaces to application

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,19 @@
 # my app.rb
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/namespace'
 require 'haml'
 
 class MyApp < Sinatra::Base
+  register Sinatra::Namespace
   get '/' do
     noname = "Eyesofbanquo"
     haml :index, :locals => {:noname => noname}
+  end
+
+  # Endpoint for retrieving rules
+  namespace '/rules' do
+    get '/assault' do
+      "Assault rules"
+    end
   end
 end
